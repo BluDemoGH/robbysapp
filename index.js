778e4c177390ac6bb99c8bdeede6b54e7eae50a2
robbyandrews <robby.andrewsandroid@gmail.com>
2022-04-14 21:56:33 -0700

    testing readme secrets again

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ aws_access_key_id1 = AKIAYVP4CIPPKCVMODKA
 aws_secret_access_key1 = 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
 output = json
 region = us-east-2
+
+
+
+// Language: javascript
+
+mypassword = "thebestpasswordcant'befound"
+
+// end of file
